e37aa39c114c7daf659e34271e44ed52aff14c03
John Reed <JohnReedLOL@ufl.edu>
2015-04-24 15:08:41 -0400

    initial commit

---
+++ Tutorial.java
@@ -0,0 +1,8 @@
+/**
+ * Getting used to the libjitsi library by writing tutorial code from 
+  * http://blog.sharedmemory.fr/en/2014/07/07/gsoc-2014-libjitsi-tutorial/
+ * @author John-Michael Reed
+ */
+public class tutorial {
+    
+}
